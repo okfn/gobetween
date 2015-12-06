48f7463c457d889ce47ce1a3a5fe6a693046706b
Paul Walsh <paulywalsh@gmail.com>
2015-12-06 13:54:38 +0200

    fix bug for file pipe.

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ import controllers from './controllers'
 let router = Router()
 
 export default function routes() {
-  router.get('*', controllers.filePipe)
+  router.get('/*', controllers.filePipe)
   router.get('*', controllers.domainPipe)
   return router
 }
